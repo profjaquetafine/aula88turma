9d545af862da558ddd118182fa500ba86650ae7b
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2022-12-08 20:11:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var mouseEvent = "empty";
+var lastPositionOfX, lastPositionOfY;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    widthOfLine = 1;
+    canvas.addEventListener("mousedown", my_mousedown);
+
+
+
+//Adicione o código MouseDown aqui
+
+
+
+
+
+
+//Adicione aqui o MouseUp
+
+
+
+//Add Mouseleave
+
+
+ 
+
+//Add MouseMove
+
+
+
+
+
+
+//Atividade Adicional
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
